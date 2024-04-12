39b959ea2b9b3754594ec7a5a474804222dc0182
Simon Kågedal Reimer <skagedal@gmail.com>
2024-04-12 23:01:31 +0200

    Attempt at reading things

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
+
+import fs from 'fs/promises'
+
+async function readPackageJson() {
+  const packageJson = await fs.readFile('package.json', 'utf8');
+  return JSON.parse(packageJson);
+}
+
 console.log('Greetings from create-program!');
+console.log('Reading package.json...');
+console.log(await readPackageJson());
