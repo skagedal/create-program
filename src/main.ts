89ec0d856ae146684d27fc00ce0963cd14d80922
Simon Kågedal Reimer <skagedal@gmail.com>
2024-04-12 23:44:28 +0200

    Do something interesting with package.json

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 
 import fs from 'fs/promises'
 
-async function readPackageJson() {
-  const packageJson = await fs.readFile('package.json', 'utf8');
-  return JSON.parse(packageJson);
+type ProjectPackageJson = {
+  name: string;
+}
+
+async function readPackageJson(): Promise<ProjectPackageJson | undefined> {
+  try {
+    const packageJson = await fs.readFile('package.json', 'utf8');
+    return JSON.parse(packageJson) as ProjectPackageJson;
+  } catch (error: any) {
+    if (error.code === 'ENOENT') {
+      return undefined;
+    } else {
+      throw error;
+    }
+  }
 }
 
 export async function main() {
     console.log('Reading package.json...');
-    console.log(await readPackageJson());    
+    const packageJson = await readPackageJson();
+    if (packageJson !== undefined) {
+      console.log('Package name:', packageJson?.name);
+    } else {
+      console.log('No package.json found');
+    }
 }
